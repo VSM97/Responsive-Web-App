a49ad03fb7550c211f18d5dda856b751cb081797
VSM97 <vsmplus2@gmail.com>
2021-12-16 19:34:42 +0530

    first

---
+++ Practice site/js.js
@@ -0,0 +1,16 @@
+function DateFunction()
+{
+    document.getElementById('date').innerHTML = Date();
+}
+function Greeting()
+{
+    document.getElementById('hello').innerHTML = 'Hello VSM !!!!';
+}
+function TurnOnBulb()
+{
+    document.getElementById('bulb').src='images/pic_bulbon.gif';
+}
+function TurnOffBulb()
+{
+    document.getElementById('bulb').src='images/pic_bulboff.gif';
+}
