df43dd8aeda29ebf03a44ad9efa8b29eeae4db44
VSM97 <vsmplus2@gmail.com>
2022-01-03 21:27:51 +0530

    Week 02
    Update 01

---
+++ SIT120-RWA-Assignments/Week02/products.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+      id: 0,
+      name: "T-shirt 1",
+      price: 29.99,
+      instock: 100,
+      description: "Mens T-shirt",
+      imgSrc: "./img/t1.png",
+    },
+    {
+      id: 1,
+      name: "T-shirt 2",
+      price: 14.99,
+      instock: 43,
+      description: "Mens T-shirt",
+      imgSrc: "./img/t2.png",
+    }
+  ];
+
