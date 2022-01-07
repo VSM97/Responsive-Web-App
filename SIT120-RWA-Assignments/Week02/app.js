526cefc1d7c52a8bc00edbcf58de7b9d55712567
VSM97 <vsmplus2@gmail.com>
2022-01-07 11:36:31 +0530

    update 0.1.3
    Cart item rendering
    will alert if item.id already  in cart

--- SIT120-RWA-Assignments/Week02/app.js
+++ SIT120-RWA-Assignments/Week02/app.js
@@ -1,5 +1,6 @@
 //Selecting Elements
 const productElements = document.querySelector(" .products");
+const cartElements = document.querySelector(" .cart-items");
 
 //Rendering productcs
 function renderProdcuts(){
@@ -8,13 +9,12 @@ function renderProdcuts(){
             <div class="item">
                 <div class="item-container">
                     <div class="item-img">
-                        <img src="./img/t1.png" alt="t-shirt 1">
+                        <img src="${product.imgSrc}" alt="${product.name}">
                     </div>
                     <div class="desc">
-                        <h2>T-shirt 1</h2>
-                        <h2><small>$</small>10.00</h2>
-                    </div>
-                    <button class="addTocart" type="button">Add to cart</button>
+                        <h2>${product.name}</h2>
+                        <h2><small>$</small>${product.price}</h2>
+                            <button class="addTocart" type="button" onclick="addToCart(${product.id})">Add to cart</button>
                     </div>
                 </div>
         `
@@ -22,3 +22,70 @@ function renderProdcuts(){
 }
 
 renderProdcuts();
+
+//Declare Cart
+let cart = [];
+
+//addToCart Function
+function addToCart(id){
+    //ignore items already in cart
+    if (cart.some((item) => item.id === id)){
+        alert("Product already added to cart!!!")
+    } else{
+        const item = products.find((product)  => product.id === id)
+        cart.push({
+            ...item,
+            numberOfUnits : 1
+        });
+    }
+
+
+    updateCart();
+}
+
+//Update Cart
+function updateCart(){
+    renderCartItems();
+    renderSubTotal();
+}
+
+//Render cart items
+function renderCartItems(){
+    cartElements.innerHTML = ""; //clear cart elements
+    cart.forEach( (item) => {
+        cartElements.innerHTML += `
+            <div class="cart-item">
+                <div class="item-info">
+                    <img src="${item.imgSrc}" alt="${item.name}">
+                    <h4>${item.name}</h4>
+                </div>
+                <div class="unit-price">
+                    <small>$</small>${item.price}
+                </div>
+                <div class="units">
+                    <button onclick="changeNumberOfUnits('minus',${item.id})"><div class="btn minus">-</div></button>
+                    <div class="number">${item.numberOfUnits}</div>
+                    <button onclick="changeNumberOfUnits('plus', ${item.id})"><div class="btn plus">+</div></button>
+                </div>
+            </div>
+            `;
+
+        });
+}
+
+//change numberOfUnits of an item
+function changeNumberOfUnits(action, id){
+    cart = cart.map( (item) => {
+
+        let oldNumberOfUnits = item.numberOfUnits;
+
+        if(item.id === id){
+            oldNumberOfUnits--
+        }else if(action === "plus"){
+            oldNumberOfUnits++;
+        }
+
+
+        return item;
+    })
+}
--- SIT120-RWA-Assignments/Week02/products.js
+++ SIT120-RWA-Assignments/Week02/products.js
@@ -14,6 +14,24 @@ const products = [
       instock: 43,
       description: "Mens T-shirt",
       imgSrc: "./img/t2.png",
+    },
+    {
+      id: 2,
+      name: "T-shirt 3",
+      price: 30.00,
+      instock: 10,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/t3.png",
+    },
+    {
+      id: 3,
+      name: "T-shirt 4",
+      price: 40.00,
+      instock: 5,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/t4.png",
     }
   ];
 
